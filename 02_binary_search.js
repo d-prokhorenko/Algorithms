269974cf03c4752a7cdd70df6ac3a7556f564635
Denis Prokhorenko <denis.prokhorenko1@gmail.com>
2024-09-18 08:38:57 +0300

    refactor: CR -> LF

--- 02_binary_search.js
+++ 02_binary_search.js
@@ -1,1 +1,48 @@
-const array = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];let count = 0;function binarySearch(array, item) {    let start = 0;    let end = array.length;    let middle;    let found = false;    let position = -1;    while(!found && start <= end) {        count++;        middle = Math.floor((start + end) / 2);        if (array[middle] === item) {            found = true;            position = middle;            return position;        }        if (item < array[middle]) {            end = middle - 1;        } else {            start = middle + 1;        }    }    return position;}function recursiveBinarySearch(array, item, start, end) {    const middle = Math.floor((start + end) / 2);    count++;    if (array[middle] === item) {        return middle;    }    if (item < array[middle]) {        return recursiveBinarySearch(array, item, start, middle - 1);    } else {        return recursiveBinarySearch(array, item, middle + 1, end);    }}// console.log(binarySearch(array, 0));console.log(recursiveBinarySearch(array, 11, 0, array.length));console.log('count =', count);
+const array = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+let count = 0;
+
+
+function binarySearch(array, item) {
+    let start = 0;
+    let end = array.length;
+    let middle;
+    let found = false;
+    let position = -1;
+
+    while(!found && start <= end) {
+        count++;
+        middle = Math.floor((start + end) / 2);
+        if (array[middle] === item) {
+            found = true;
+            position = middle;
+            return position;
+        }
+
+        if (item < array[middle]) {
+            end = middle - 1;
+        } else {
+            start = middle + 1;
+        }
+    }
+
+    return position;
+}
+
+function recursiveBinarySearch(array, item, start, end) {
+    const middle = Math.floor((start + end) / 2);
+    count++;
+
+    if (array[middle] === item) {
+        return middle;
+    }
+
+    if (item < array[middle]) {
+        return recursiveBinarySearch(array, item, start, middle - 1);
+    } else {
+        return recursiveBinarySearch(array, item, middle + 1, end);
+    }
+}
+
+// console.log(binarySearch(array, 0));
+console.log(recursiveBinarySearch(array, 11, 0, array.length));
+console.log('count =', count);
--- 06_recursion.js
+++ 06_recursion.js
@@ -1,1 +1,35 @@
-function factorial(number) {    if (number <= 1) {        return number;    }    return number * factorial(number - 1);}console.log(factorial(5));// ----------------------------------------------------------function fibonachi(n) {    if (n <= 1) {        return n;    }    return fibonachi(n - 1) + fibonachi(n - 2);}function fib(n) {    let a = 1;    let b = 1;    for (let i = 3; i <= n; i++) {        let c = a + b;        a = b;        b = c;    }    return b;}console.log(fibonachi(7));console.log(fib(7));
+function factorial(number) {
+    if (number <= 1) {
+        return number;
+    }
+
+    return number * factorial(number - 1);
+}
+
+console.log(factorial(5));
+
+// ----------------------------------------------------------
+
+function fibonachi(n) {
+    if (n <= 1) {
+        return n;
+    }
+
+    return fibonachi(n - 1) + fibonachi(n - 2);
+}
+
+function fib(n) {
+    let a = 1;
+    let b = 1;
+
+    for (let i = 3; i <= n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return b;
+}
+
+console.log(fibonachi(7));
+console.log(fib(7));
